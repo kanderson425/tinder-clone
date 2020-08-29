a3e91f678944d02f7bd97cbe449362323e9e1666
kanderson425 <kyleanderson.dev@gmail.com>
2020-08-29 14:32:22 -0500

    Adds the basic wireframe of the Header component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Let's build the TINDER Clone App</h1>
+    <div className="app">
+    <h1>Let's build the Tinder Clone App</h1>
+      <Header />
+      {/* Tinder Cards */ }
+      {/* Buttons below tinder cards */}
+
+
+      {/* Chats Screen */ }
+      {/* Individual Chat Screen */}
+
+    </div>
   );
 }
 
